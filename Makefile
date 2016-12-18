39ce11b700c41ed437ff0f97f416ab86248826f5
caodac <caodac@gmail.com>
2016-12-18 09:26:08 -0500

    add lmdb; work on small dataset but failed on full

--- Makefile
+++ Makefile
@@ -2,10 +2,12 @@
 CC = clang
 MPICC = mpicc
 DEBUG = -g
-CFLAGS = -Wall $(DEBUG)
-OBJS = sparse.o sparse_mpi.o
+LMDB = /usr/local/lmdb/0.9.16
+CFLAGS = -Wall $(DEBUG) 
+LIBS = -L$(LMDB)/lib -llmdb
+OBJS = sparse.o 
 
-TARGETS = sparse_mpi sparse_test
+TARGETS = sparse_mpi sparse_mpi_mdb sparse_test
 
 all: $(TARGETS)
 
@@ -15,11 +17,17 @@ all: $(TARGETS)
 sparse_mpi.o: sparse_mpi.c
 	$(MPICC) $(CFLAGS) -c $<
 
-sparse_mpi: $(OBJS)
-	$(MPICC) $(CFLAGS) -o $@ $(OBJS)
+sparse_mpi_mdb.o: sparse_mpi.c
+	$(MPICC) $(CFLAGS) -DHAVE_LMDB -I$(LMDB)/include -o $@ -c $<
+
+sparse_mpi: $(OBJS) sparse_mpi.o
+	$(MPICC) $(CFLAGS) -o $@ $(OBJS) sparse_mpi.o
+
+sparse_mpi_mdb: $(OBJS) sparse_mpi_mdb.o
+	$(MPICC) $(CFLAGS) -o $@ $(OBJS) sparse_mpi_mdb.o $(LIBS)
 
 sparse_test: sparse.c
 	$(CC) $(CFLAGS) -DSPARSE_MAIN -o $@ $<
 
 clean:
-	rm -f $(OBJS) $(TARGETS)
+	rm -f *.o $(TARGETS)
--- sparse_mpi.c
+++ sparse_mpi.c
@@ -1,8 +1,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 #include <mpi.h>
 #include <assert.h>
+
+#ifdef HAVE_LMDB
+#  include <lmdb.h>
+#endif
 
 #include "sparse.h"
 
@@ -14,40 +19,89 @@ struct node_s {
 int
 main (int argc, char *argv[])
 {
-  int err, rank, numtasks, len;
+  int err = 1, rank, numtasks, len;
   long total = 0l;
   sparse_reader_t *reader;
-  struct node_s *head, *node, *next;
+  struct node_s *head = 0, *node, *next;
   char hostname[MPI_MAX_PROCESSOR_NAME]= {0};
+
+#ifdef HAVE_LMDB
+  MDB_env *env = 0;
+  MDB_txn *txn = 0;
+  MDB_dbi dbi = 0;
+#else  
   char outfile[BUFSIZ];
   FILE *outfp;
+#endif  
   
   MPI_Init (&argc, &argv);
   
   MPI_Comm_rank (MPI_COMM_WORLD, &rank);
   MPI_Comm_size (MPI_COMM_WORLD, &numtasks);
   MPI_Get_processor_name (hostname, &len);
-  
-  if (argc == 1)
-    {
-      fprintf (stderr, "Error: No sparse input file specified!\n");
-      return 1;
-    }
-      
+
   reader = sparse_reader_create ();
-  err = sparse_reader_open (reader, argv[1]);
+  assert (reader != 0);
+  
+#ifdef HAVE_LMDB  
+  if (argc < 3)
+    {
+      fprintf (stderr, "Usage: %s DBDIR FILE\n", argv[0]);
+      goto done;
+    }
+  
+  err = mdb_env_create (&env);
   if (err)
     {
-      fprintf (stderr, "Error: %s\n", sparse_reader_error (reader));
-      sparse_reader_destroy (reader);
-      return 1;
+      fprintf (stderr, "Error: can't create db env: %s\n", mdb_strerror (err));
+      goto done;
+    }
+
+  err = mdb_env_open (env, argv[1], MDB_FIXEDMAP, 0664);
+  if (err)
+    {
+      fprintf (stderr, "Error: can't open db path: %s (%s)\n",
+               argv[1], mdb_strerror (err));
+      goto done;
+    }
+
+  err = mdb_txn_begin (env, 0, 0, &txn);
+  if (err)
+    {
+      fprintf (stderr, "Rank=%d: Error: can't start transaction (%s)\n",
+	       rank, mdb_strerror (err));
+      goto done;
+    }
+
+  err = mdb_dbi_open (txn, 0, MDB_CREATE | MDB_INTEGERKEY, &dbi);
+  if (err)
+    {
+      fprintf (stderr, "Rank=%d: Error: can't open database (%s)\n", 
+	       rank, mdb_strerror (err));
+      goto done;
+    }
+  
+  err = sparse_reader_open (reader, argv[2]);
+#else
+  if (argc == 1)
+    {
+      fprintf (stderr, "Usage: %s FILE\n", argv[0]);
+      goto done;
     }
 
   (void) sprintf (outfile, "sparse_mpi-%s-%d.txt", hostname, rank);
   outfp = fopen (outfile, "w");
   assert (outfp != 0);
+  
+  err = sparse_reader_open (reader, argv[1]);  
+#endif /* !HAVE_LMDB */
+  
+  if (err)
+    {
+      fprintf (stderr, "Error: %s\n", sparse_reader_error (reader));
+      goto done;
+    }
 
-  head = 0;
   while (sparse_reader_next (reader) > 0)
     {
       struct node_s *n = malloc (sizeof (*node));
@@ -61,14 +115,52 @@ main (int argc, char *argv[])
             {
               sparse_vector_t *contrib;
               double sim = sparse_vector_tanimoto (next->v, n->v, &contrib);
+#ifdef HAVE_LMDB
+	      uint64_t id;
+	      MDB_val key, val;
 
+	      if (next->v->id < n->v->id)
+		{
+		  id  = next->v->id;
+		  id = (id << 32l) | n->v->id;
+		}
+	      else
+		{
+		  id = n->v->id;
+		  id = (id << 32l) | next->v->id;
+		}
+
+	      if (total % 1000 == 0)
+		{
+		  err = mdb_txn_commit (txn);
+		  if (err)
+		    fprintf (stderr, "Rank=%d: Error: commiting transaction at record %ld (%s)\n",
+			     rank, total, mdb_strerror (err));
+
+		  err = mdb_txn_begin (env, 0, 0, &txn);
+		  if (err)
+		    fprintf (stderr, "Rank=%d: Error: can't start transaction at record %ld (%s)\n",
+			     rank, total, mdb_strerror (err));
+		}
+
+	      key.mv_size = sizeof (id);
+	      key.mv_data = &id;
+	      val.mv_size = sizeof (sim);
+	      val.mv_data = &sim;
+	      err = mdb_put (txn, dbi, &key, &val, 0);
+	      if (err)
+		{
+		  fprintf (stderr, "Rank=%d: Error: put at record %ld (%s)\n", 
+			   rank, total, mdb_strerror (err));
+		}
+#else
               fprintf (outfp, "%d %d %lf", next->v->id, n->v->id, sim);
               for (len = 0; len < contrib->size; ++len)
                 fprintf (outfp, " %d:%f", contrib->data[len].pos,
                          contrib->data[len].value);
               fputc ('\n', outfp);
               //printf ("%d: (%d,%d)\n", rank, next->v->id, n->v->id);
-
+#endif /* !HAVE_LMDB */
               free (contrib);
               ++total;
             }
@@ -80,8 +172,22 @@ main (int argc, char *argv[])
         node->next = n;
       node = n;
     }
-  printf ("** %s: rank=%d total=%ld\n", outfile, rank, total);
+
+#ifdef HAVE_LMDB
+  err = mdb_txn_commit (txn);
+  if (err)
+    fprintf (stderr, "Rank=%d: Error: commiting transaction at record %ld (%s)\n",
+	     rank, total, mdb_strerror (err));
+#endif
+
+ done:
+#ifdef HAVE_LMDB
+  printf ("** rank=%d total=%ld\n", rank, total);  
+  mdb_env_close (env);
+#else
+  printf ("** %s: rank=%d total=%ld\n", outfile, rank, total);  
   fclose (outfp);
+#endif
 
   /* clean up */
   while (head != 0)
@@ -96,5 +202,5 @@ main (int argc, char *argv[])
   
   MPI_Finalize();
   
-  return 0;
+  return err;
 }
